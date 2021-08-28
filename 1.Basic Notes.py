ee2b0b2a6bfc835585f460ddaf5fda7952bb63e6
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-28 16:26:42 +0530

    Create 1.Basic Notes.py

---
+++ 1.Basic Notes.py
@@ -0,0 +1,88 @@
+
+print(‘hello world’)
+
+Divide( / or //)
+12/3 = 4.0 (float value)
+12//3=4  	(Integer value)
+
+Exponents or power(**)
+2**2=4
+2**3=8
+
+Root
+49**(1/2)=7.0
+49**(1//2)=7
+
+Remaindar(%)
+7%3=1
+
+ Strings
+'he is a "good" guy'                    ->    'he is a "good" guy'
+ "he's a good guy"                      ->     "he's a good guy"
+'he\'s a good guy'                       ->     "he's a good guy"
+
+User Input
+String
+input('Please enter a string)
+->Please enter a stringarushi
+‘arushi’
+
+Number
+input('Please enter a value')
+->Please enter a value2
+‘2’
+
+
+New Line(\n)
+print('hi mike \nwelcome to club')
+->hi mike
+->welcome to club
+
+Operations on String
+ "5"+"5"   =   '55'
+ 5+5=10
+"Hello"+"5"  = 'hello5'
+"helo"*5  =  'helohelohelohelohelo'
+
+Variables
+Storing value
+>>> a=10
+>>> print(a)
+10
+>>> b=20
+>>> print(b)
+20
+>>> c=a+b
+>>> print(c)
+30
+
+Storing String
+>>> s="arushi"
+>>> print(s)
+arushi
+
+To remove data in String(del)
+>>> del s
+>>> print(s)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 's' is not defined
+
+In place operator
+>>> age=30
+>>> age+=1
+>>> print(age)
+31
+>>> age-=2
+>>> print(age)
+29
+>>> age*=2
+>>> print(age)
+58
+>>> b="hello"
+>>> b+="world"
+>>> print(b)
+helloworld
+
+
+
