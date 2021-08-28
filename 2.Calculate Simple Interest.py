fc0c830dcef7acf0b83312b645bd24816106c88a
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-28 16:35:45 +0530

    Simple Interest

---
+++ 2.Calculate Simple Interest.py
@@ -0,0 +1,10 @@
+p = input('Enter a value for p')
+r = input('Enter a value for r')
+t = input('Enter a value for t')
+
+p = int(p)
+r = int(r)
+t = int(t)
+
+simpleInterest = (p * r * t) / 100
+print(simpleInterest)
