587753a941273100b9bb1e942688331d34b31381
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 17:20:11 +0530

    Create 9.Loops.py

---
+++ 9.Loops.py
@@ -0,0 +1,44 @@
+*For Loop
+
+To print loop in range
+for x in range(1,11):
+   print(x)
+o/p-1
+2
+3
+4
+5
+6
+7
+8
+9
+
+To print list using loop 
+fruits =["apple","peach","banana"]
+for x in fruits:
+   print(x)
+o/p-apple
+peach
+banana
+
+To print  even numbers from zero to twenty
+for x in range(0,21,2):
+   print(x)
+
+    
+    
+*While Loop
+counter=0
+while(counter<10):
+   print(counter)
+   counter+=1
+o/p-0
+1
+2
+3
+4
+5
+6
+7
+8
+9
