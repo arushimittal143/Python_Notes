61f162254e4024f3cd49efaba4cd02d761fb17f1
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 16:06:12 +0530

    Create 5.List Operations.py

---
+++ 5.List Operations.py
@@ -0,0 +1,17 @@
+numbers=[1,1,1,1,1]
+numbers[2]=3
+print(numbers)
+o/p-[1, 1, 3, 1, 1]
+
+newNumbers=[1,2,3,4,5]
+print(numbers+newNumbers)
+o/p-[1, 1, 3, 1, 1, 1, 2, 3, 4, 5]
+
+print(numbers*3)
+o/p-[1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1]
+
+fruits=["Apple","Mango","Guava"]
+print("Apple" in fruits)
+print("Orange" in fruits)
+o/p-True
+False
