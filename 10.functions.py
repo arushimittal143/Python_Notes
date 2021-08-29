06ca76441d086fe70965a8ee0792c42bd7c5b650
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 18:29:02 +0530

    Update 10.functions.py

--- 10.functions.py
+++ 10.functions.py
@@ -8,3 +8,30 @@ o/p-
 hii
 how are you
 fine
+
+
+**Passing arguments to functions
+def add(a,b):
+   sum=a+b;
+   print(sum)
+
+add(10,20)
+add(30,40)
+
+
+**Return some value in Function
+def add(a,b):
+   c=a*b;
+   return c;
+
+print(add(10,20))
+
+**Passing functions as argument
+def add(a,b):
+   return a+b
+
+def square(c):
+   return c*c
+
+result=square(add(2,3))
+print(result)
