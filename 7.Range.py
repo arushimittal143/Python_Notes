32b422123da4cf0596fdfe7ffed5260208363c79
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 16:27:00 +0530

    Create 7.Range.py

---
+++ 7.Range.py
@@ -0,0 +1,11 @@
+numbers=list(range(10))
+print(numbers)
+o/p-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+numbers=list(range(3,8))
+print(numbers)
+o/p-[3, 4, 5, 6, 7]
+
+numbers=list(range(0,30,3))
+print(numbers)
+o/p-[0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
