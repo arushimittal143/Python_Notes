6f9b8bc82d44f19b0d3d1bfa5b5fbdbe0f88589b
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 17:17:54 +0530

    Create 8.Boolean.py

---
+++ 8.Boolean.py
@@ -0,0 +1,19 @@
+And 
+username="admin"
+password="admin123"
+
+if username=="admin" and password=="admin123":
+   print("Valis User")
+else:
+   print("Invalid User")
+o/p-Valis User
+
+Or
+username="user"
+password="admin123"
+
+if username=="admin" or password=="admin123":
+   print("Valis User")
+else:
+   print("Invalid User")
+o/p-Valid User
