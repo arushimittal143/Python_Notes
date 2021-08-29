dd1508cd9435a18c0a2919c86872bb049605c59c
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 16:18:29 +0530

    Create 6.Lists Functions.py

---
+++ 6.Lists Functions.py
@@ -0,0 +1,22 @@
+To add value in list (value will be stored at end)
+fruits=["Apple","Mango","Guava","Oranges"]
+fruits.append("Banana")
+print(fruits)
+o/p-['Apple', 'Mango', 'Guava', 'Oranges', 'Banana']
+
+To find the length of list
+print(len(fruits))
+o/p-5
+
+
+
+
+To insert a value at particular index
+fruits.insert(1,"Strawberry")
+print(fruits)
+o/p-['Apple', 'Strawberry', 'Mango', 'Guava', 'Oranges', 'Banana']
+
+
+To know the index of particular value
+print(fruits.index("Guava"))
+o/p-3
