52afd1eef82048a2789f9111d688c80f19f32292
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 12:45:25 +0530

    Create 3.Conditional Statements.py

---
+++ 3.Conditional Statements.py
@@ -0,0 +1,20 @@
+If Else Statement
+age = int(input('enter your age'))
+
+if age >= 18:
+   print("You are an adult")
+else:
+   print("You are not an adult")
+
+    
+If Elif Statement
+marks=int(input("Enter your marks")
+          
+if marks>=90:
+   print("Grade A")
+elif marks>=70:
+   print("Grade B");
+elif marks>=60:
+   print("Grade C")
+elif marks<60:
+   print("Grade D")
