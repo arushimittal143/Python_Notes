be02b45c1ff02ed333d25c0c567d2db800ae5535
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-08-29 12:46:08 +0530

    Create 4.List.py

---
+++ 4.List.py
@@ -0,0 +1,20 @@
+List of String
+names=["Mike","John","Rob"]
+print(names)
+print(names[0])
+O/p-['Mike', 'John', 'Rob']
+Mike
+
+
+List Of Numbers
+numbers=[1,2,3,4,5]
+print(numbers)
+print(numbers[1])
+O/p-[1, 2, 3, 4, 5]
+2
+
+Empty List
+emptyList=[]
+print(emptyList)
+o/p-[]
+
