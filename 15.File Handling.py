7e1fe6f067323c7f8718e593c2d632354babb22d
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-09-05 12:09:47 +0530

    Update 15.File Handling.py

--- 15.File Handling.py
+++ 15.File Handling.py
@@ -21,3 +21,50 @@ file.close()
 O/p-this is the first line
 
 
+#Writing and reading file
+file = open("demo.txt", "w")
+file.write("this is the text written into the file")
+file.close()
+
+file = open("demo.txt", "r")
+content=file.read();
+print(content)
+#O/p-(console)this is the text written into the file
+
+#Demo.txt
+#this is the text written into the file
+
+#Writing and reading and then writing-->In this the content written in first write gets deleted
+file = open("demo.txt", "w")
+file.write("this is the text written into the file")
+file.close()
+
+file = open("demo.txt", "r")
+content=file.read();
+print(content)
+
+file = open("demo.txt", "w")
+file.write("this is the new text")
+file.close()
+
+#O/p-(console)this is the text written into the file
+#Demo.txt
+#this is the new text
+
+#Appending the content in the file
+file = open("demo.txt", "w")
+file.write("this is the text written into the file")
+file.close()
+
+file = open("demo.txt", "r")
+content=file.read();
+print(content)
+
+file = open("demo.txt", "a")
+file.write("this is the new text")
+file.close()
+#O/p-(console)this is the text written into the file
+#Demo.txt
+#this is the text written into the filethis is the new text
+
+
