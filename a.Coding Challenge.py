57d7d7f5af6e66a0b9e4d4945b985d23190c09ab
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-09-05 12:21:46 +0530

    Create a.Coding Challenge.py

---
+++ a.Coding Challenge.py
@@ -0,0 +1,12 @@
+def divide(a, b):
+    try:
+        return (a // b)
+    except ZeroDivisionError:
+         print("This is division by zero error")
+         return 0;
+
+a=int(input("First number"))
+b=int(input("Second number"))
+
+result=divide(a, b)
+print(result)
