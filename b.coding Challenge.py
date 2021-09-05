c7f6c84a18cd75cbc8a0daacad6ebf9b239bd0af
Arushi <arushi1881.cse18@chitkara.edu.in>
2021-09-05 12:26:14 +0530

    Update b.coding Challenge.py

--- b.coding Challenge.py
+++ b.coding Challenge.py
@@ -1,3 +1,12 @@
+#1. Write Python code to open a file named demo.txt and write some random data into it.
+
+#2. Open the file, read the contents and display them as output.
+
+#3. Write python code to add additional text to the existing file on a new line without deleting the previous contents.
+
+
+
+
 file = open("demo.txt", "w")
 file.write("This is first line")
 file.close()
